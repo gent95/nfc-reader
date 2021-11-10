ae9ffeddf8dab4aa3eec331fc226946cbdae81f4
majt <majt@vandh.org>
2021-11-10 15:07:56 +0800

    每一步都好艰难

--- securityReader.py
+++ securityReader.py
@@ -11,14 +11,21 @@ from smartcard.ATR import ATR
 from Crypto.Cipher import DES3 as des3
 import random as rnd
 import time as tm
+import matplotlib.pyplot as plt
+import math
 
-card_type = AnyCardType()
-card_request = CardRequest(timeout=1, cardType=card_type)
-card_service = card_request.waitforcard()
+def init():
+  print('初始化读卡器')
+  card_type = AnyCardType()
+  card_request = CardRequest(timeout=1, cardType=card_type)
+  card_service = card_request.waitforcard()
+  card_service.connection.connect()
+  atr = ATR(card_service.connection.getATR())
+  return card_service
 
-card_service.connection.connect()
-atr = ATR(card_service.connection.getATR())
-
+# 结果打印（正确：绿色  不正确：红色）
+def result_print(result_out_str, result):
+  print(result_out_str)
 
 def trace_command(apdu):
     print('sending ', toHexString(apdu))
@@ -33,10 +40,14 @@ def trace_response(response, sw1, sw2):
         "%x %x" % (sw1, sw2)
     )
 
-    
+def bytes2int(data_bytes):
+  data_number = len(data_bytes)
+  data_int = 0
+  for i in range(0, data_number):
+    data_int = data_int + data_bytes[data_number-i-1]*math.pow(2, i*8)
+  return int(data_int)   
     
 def item_print(item_str):
-  item_str = "\033[44m" + item_str +  "\033[0m"
   print(item_str)
 
 def int2bytes(data_int):
@@ -113,7 +124,8 @@ def command_add_package_length(data_bytes):
   data_length = len(data_bytes)
   data_bytes = data_bytes[0:1] + bytes([(data_length>>8)&0xFF]) + bytes([data_length&0xFF]) + data_bytes[3:]
   return data_bytes
-  
+
+#发送指令 
 def sendCommand(command):
     trace_command(command)
     res, s1, s2 = card_service.connection.transmit(command)
@@ -122,6 +134,7 @@ def sendCommand(command):
 
 
 def cosAccess(KEYA_str, KEYB_str):
+    print('开始ACCESS验证')
     rf_command_bytes = [ 0xA2, 0xA4, 0x00, 0x0C, 0x02, 0xAC, 0x01]
     sendCommand(rf_command_bytes)
     rf_command_bytes = [0xA2, 0xB0, 0x00, 0x10, 0x10]
@@ -158,6 +171,23 @@ def cosAccess(KEYA_str, KEYB_str):
 
     return Des3_Cipher
 
+# 读读取数据
+def COS_Read_Data(Des3_Cipher, address_int, length_int, package_size):
+  item_print("COS读取数据")
+  result = False
+  # 选中PL DATA
+  rf_command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xDA,0x01]
+  data_bytes,s1,s2 = sendCommand(rf_command_bytes)
+  # 读数据
+  data_bytes = ISO14443_4A_ReadBinary(0xA2, address_int, length_int, package_size)
+  data_bytes = b''.join(map(lambda d:int.to_bytes(d, 1, 'little'), data_bytes))
+  data_bytes = Des3_Cipher.decrypt(data_bytes)
+  data_str = bytes2hexstr(data_bytes)
+  result = True
+  result_print("成功", result)
+  result_print(data_str, result)
+  return result, data_bytes
+
 def cosReadTempture(Des3_Cipher):
   command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xDA,0x01]
   res,s1,s2 = sendCommand(command_bytes)
@@ -169,15 +199,9 @@ def cosReadTempture(Des3_Cipher):
   tempture_int = tempture_int >> 5
   print(tempture_int)
 
-def cosReadData(Des3_Cipher, address_int, length_int, package_size):
-  command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xDA,0x01]
-  sendCommand(command_bytes)
-  data_bytes = iso14443a4aReadBinary(0xA2, address_int, length_int, package_size)
-  print(data_bytes)
-
-
-def iso14443a4aReadBinary(cla_bytes, address_start_int, length_int, RF_PACKAGE_SIZE):
-  data_read_bytes = b''
+# 读数据
+def ISO14443_4A_ReadBinary(cla_bytes, address_start_int, length_int, RF_PACKAGE_SIZE):
+  data_read_bytes = []
   des3_key_int_list = [0 for i in range(2)]
   package_size_int = RF_PACKAGE_SIZE
   package_number_int = int(length_int / package_size_int)
@@ -185,38 +209,31 @@ def iso14443a4aReadBinary(cla_bytes, address_start_int, length_int, RF_PACKAGE_S
   i = 0
   # 读取整包数据
   for i in range (0, package_number_int):
-    for retry_cnt_int in range (0, 10):
-      data_address_int = address_start_int + i * package_size_int
-      des3_key_int_list[0] = int(data_address_int/256)
-      des3_key_int_list[1] = int(data_address_int%256)
-      data_address_bytes = int_list2bytes(des3_key_int_list)
-      rf_command_bytes = [cla_bytes,0xB0]
-      rf_command_bytes.extend(data_address_bytes)
-      rf_command_bytes.extend(bytes([package_size_int]))
-      res,s1,s2 = sendCommand(rf_command_bytes)
-      if s1 == 144 and s2 == 0:
-        continue
-      else:
-        data_read_bytes = data_read_bytes + res[1:1+package_size_int]
-        break
-    if retry_cnt_int==9:
-      return b''
-
-def cosAnalysis(Des3_Cipher,data_size):
-  rf_command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xDA,0x01]
-  res,s1,s2 = sendCommand(rf_command_bytes)
-  iso14443a4aReadBinary(0xA2,0,80,data_size)
-  # data_bytes = Des3_Cipher.decrypt(data_bytes)
-
-
-
-Des3_Cipher = cosAccess('0xA0 0xA1 0xA2 0xA3 0xA4 0xA5 0xA6 0xA7','0xA8 0xA9 0xAA 0xAB 0xAC 0xAD 0xAE 0xAF')
-temp_num = cosReadTempture(Des3_Cipher)
-cosAnalysis(Des3_Cipher,temp_num)
-
-
-
-
+    data_address_int = address_start_int + i * package_size_int
+    des3_key_int_list[0] = int(data_address_int/256)
+    des3_key_int_list[1] = int(data_address_int%256)
+    data_address_bytes = int_list2bytes(des3_key_int_list)
+    rf_command_bytes = [cla_bytes,0xB0]
+    rf_command_bytes.extend(data_address_bytes)
+    rf_command_bytes.extend(bytes([package_size_int]))
+    data_bytes, s1,s2 = sendCommand(rf_command_bytes)
+    if s1 == 144 and s2 == 0:
+      data_read_bytes.extend(data_bytes)
+  # 读取剩余数据
+  if package_bytes_left_int!=0:
+    if package_number_int!=0:
+        i = i+1
+    data_address_int = address_start_int + i * package_size_int
+    des3_key_int_list[0] = int(data_address_int/256)
+    des3_key_int_list[1] = int(data_address_int%256)
+    data_address_bytes = int_list2bytes(des3_key_int_list)
+    rf_command_bytes = [cla_bytes,0xB0]
+    rf_command_bytes.extend(data_address_bytes)
+    rf_command_bytes.extend(bytes([package_size_int]))
+    data_bytes, s1,s2 = sendCommand(rf_command_bytes)
+    if s1 == 144 and s2 == 0:
+      data_read_bytes.extend(data_bytes)
+  return data_read_bytes
 
 
 # 数据解析
@@ -224,10 +241,11 @@ def COS_Analysis(Des3_Cipher, DATA_SIZE, show_picture_str):
   item_print("COS数据解析")
   # 选中DATA
   rf_command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xDA,0x01]
-  res,s1,s2 = sendCommand(rf_command_bytes)
-  iso14443a4aReadBinary(0xA2,0,80,DATA_SIZE)
+  data_bytes,s1,s2 = sendCommand(rf_command_bytes)
+  # 读取数据
+  data_bytes = ISO14443_4A_ReadBinary(0xA2, 0, 80, DATA_SIZE)
+  data_bytes = b''.join(map(lambda d:int.to_bytes(d, 1, 'little'), data_bytes))
   data_bytes = Des3_Cipher.decrypt(data_bytes)
-
   # 解析数据
   data_bias = 12
   CID_bytes  = data_bytes[data_bias+0: data_bias+2]
@@ -263,7 +281,8 @@ def COS_Analysis(Des3_Cipher, DATA_SIZE, show_picture_str):
   if(record_data_size_mod_int):
     record_data_size_int = record_data_size_int + (8-record_data_size_mod_int)
   # 读取数据
-  data_bytes = ISO14443_4A_ReadBinary(SeialCom, b'\xA2', 78, record_data_size_int, DATA_SIZE)
+  data_bytes = ISO14443_4A_ReadBinary(0xA2, 78, record_data_size_int, DATA_SIZE)
+  data_bytes = b''.join(map(lambda d:int.to_bytes(d, 1, 'little'), data_bytes))
   data_bytes = Des3_Cipher.decrypt(data_bytes)
   # 解析adc
   tempture_data = [0 for i in range(record_number_int)]
@@ -306,7 +325,7 @@ def COS_Analysis(Des3_Cipher, DATA_SIZE, show_picture_str):
       tempture_data[decode_count-1] = adc_data_int
   result_print(str(tempture_data), True)
   # 波形显示
-  if show_picture_str == "show picture":
+  if show_picture_str:
     DATA_FILTER = 0
     x = [0 for i in range(record_number_int)]
     for i in range(0, record_number_int):
@@ -322,51 +341,48 @@ def COS_Analysis(Des3_Cipher, DATA_SIZE, show_picture_str):
     plt.show()
 
 
-def ISO14443_4A_ReadBinary(sscom, cla_bytes, address_start_int, length_int, RF_PACKAGE_SIZE):
-  data_read_bytes = b''
-  des3_key_int_list = [0 for i in range(2)]
-  package_size_int = RF_PACKAGE_SIZE
-  package_number_int = int(length_int / package_size_int)
-  package_bytes_left_int = length_int % package_size_int
-  i = 0
-  # 读取整包数据
-  for i in range (0, package_number_int):
-    for retry_cnt_int in range (0, 10):
-      data_address_int = address_start_int + i * package_size_int
-      des3_key_int_list[0] = int(data_address_int/256)
-      des3_key_int_list[1] = int(data_address_int%256)
-      data_address_bytes = int_list2bytes(des3_key_int_list)
-      rf_command_bytes = iso14443a_add_crc16(b'\x02' + cla_bytes + b'\xB0' + data_address_bytes + bytes([package_size_int]))
-      command_bytes = build_com_command(rf_command_bytes)
-      data_bytes, length = sscom_transceive_bytes(sscom, command_bytes)
-      if data_bytes[-4:-2] != b'\x90\x00':
-        continue
-      else:
-        data_read_bytes = data_read_bytes + data_bytes[1:1+package_size_int]
-        break
-    if retry_cnt_int==9:
-      return b''
-  # 读取剩余数据
-  if package_bytes_left_int!=0:
-    if package_number_int!=0:
-        i = i+1
-    for retry_cnt_int in range (0, 10):
-      data_address_int = address_start_int + i * package_size_int
-      des3_key_int_list[0] = int(data_address_int/256)
-      des3_key_int_list[1] = int(data_address_int%256)
-      data_address_bytes = int_list2bytes(des3_key_int_list)
-      rf_command_bytes = iso14443a_add_crc16(b'\x02' + cla_bytes + b'\xB0' + data_address_bytes + bytes([package_bytes_left_int]))
-      command_bytes = build_com_command(rf_command_bytes)
-      data_bytes, length = sscom_transceive_bytes(sscom, command_bytes)
-      if data_bytes[-4:-2] != b'\x90\x00':
-        continue
-      else:
-        data_read_bytes = data_read_bytes + data_bytes[1:1+package_bytes_left_int]
-        break
-    if retry_cnt_int==9:
-      return b''
-  return data_read_bytes
+def COS_Read_Config(Des3_Cipher, address_int, length_int, package_size):
+  item_print("COS读取配置数据")
+  result = False
+  # 选中CONFIG
+  rf_command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xCF,0x01]
+  command_bytes = build_com_command(rf_command_bytes)
+  data_bytes, s1,s2  = sendCommand(rf_command_bytes)
+  # 读数据
+  data_bytes = ISO14443_4A_ReadBinary(0xA2, address_int, length_int, package_size)
+  data_bytes = b''.join(map(lambda d:int.to_bytes(d, 1, 'little'), data_bytes))
+  data_bytes = Des3_Cipher.decrypt(data_bytes)
+  data_str = bytes2hexstr(data_bytes)
+  result = True
+  result_print("成功", result)
+  result_print(data_str, result)
+  return result, data_bytes
+
+
+def COS_Write_Config(Des3_Cipher, address_int, length_int, data_hex_str, package_size):
+  item_print("COS写入配置数据")
+  result = False
+  # 选中CONFIG
+  rf_command_bytes = iso14443a_add_crc16(b'\x02\xA2\xA4\x00\x0C\x02\xCF\x01')
+  rf_command_bytes = [0xA2,0xA4,0x00,0x0C,0x02,0xCF,0x01]
+  command_bytes = build_com_command(rf_command_bytes)
+  data_bytes, length = sscom_transceive_bytes(SeialCom, command_bytes)
+  data_write_str = data_hex_str
+  data_write_bytes = hexstr2bytes(data_write_str)
+  data_write_bytes = Des3_Cipher.encrypt(data_write_bytes)
+  data_bytes = ISO14443_4A_UpdateBinary(SeialCom, b'\xA2', address_int, length_int, data_write_bytes, package_size)
+  if data_bytes!=b'':
+    result = True
+    result_print("成功", result)
+  if result == False:
+    result_print("失败", result)
+  return result
 
 
 
 
+
+card_service = init()
+Des3_Cipher = cosAccess('0xA0 0xA1 0xA2 0xA3 0xA4 0xA5 0xA6 0xA7','0xA8 0xA9 0xAA 0xAB 0xAC 0xAD 0xAE 0xAF')
+# temp_num = cosReadTempture(Des3_Cipher)
+COS_Analysis(Des3_Cipher,60,False)
